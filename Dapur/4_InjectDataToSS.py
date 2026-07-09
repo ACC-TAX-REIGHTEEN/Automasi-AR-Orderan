3b521f3b2a9ce8d138d27ac7557540d6492a6d2e
Ihfandi <39288119+ihfandica@users.noreply.github.com>
2026-07-09 20:52:46 +0700

    Add some projections

--- Dapur/4_InjectDataToSS.py
+++ Dapur/4_InjectDataToSS.py
@@ -1,8 +1,9 @@
 Otak yang akan memindai data di spreadsheets orderan berdasarkan nomor pelanggan dan menggunakan fungsi perbatch untuk menghemat API
 Menyisipkan total piutang, tidak termasuk fraud, menyisipkan keterangan owing dan JT jika ada. harapan isi:
-SL-1151	EKA SAKTI	PCMO (dari excel AR)
+SL-1151	EKA SAKTI	PCMO (dari excel AR) berikan fuzzywuzzy agar sedikit typo teratasi
 07/07/2026		(ambil tanggal masuk orderan)
-		
+
+berikan kemampuan untuk skip salah satu dari aravg, dll dengan config.conf
 Piutang	 30.002.000 	(total yang sama dengan kalkulasi piutang, tidak termasuk fraud)
 Inv	 4 	(total faktur)
 Plafon	 13.800.000 	(ambil dari excel hasil unduhan spreadsheets untuk AVG AR)
@@ -11,3 +12,6 @@ Rata" History Bayar	99	Hari (ambil dari excel hasil unduhan spreadsheets untuk A
 
 260600522	 5.470.400 	 33 	HR (JT 08/07/26) (dari excel AR + hasl helper giro DD/MM/YY)
 260605984	 7.654.100 	 10 	HR (contoh biasa tanpa giro)
+
+berikan kemampuan untuk target url dan sheet jika dalam satu url terdapat beberapa sheet
+berikan kemampuan untuk skip owing jika user memang tidak mengunduhnya
